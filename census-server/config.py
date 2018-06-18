2ba0dd85df8d976ea0ec97d90480efb9da8018e2
Stephen Smoogen <smooge@gmail.com>
2018-06-18 21:58:21 +0000

    put in the requirements.txt

--- census-server/config.py
+++ census-server/config.py
@@ -6,12 +6,22 @@ This module is responsible for loading the application configuration.
 
 """
 
-GEO_DB_COUNTRY_LOC = '/home/smooge/GeoIP2/GeoIP2-Country.mmdb'
-GEO_DB_DOMAIN_LOC = '/home/smooge/GeoIP2/GeoIP2-Domain.mmdb'
-GEO_DB_ISP_LOC = '/home/smooge/GeoIP2/GeoIP2-ISP.mmdb'
+import os
+#basedir = os.path.abspath(os.path.dirname(__file__))
 
+basedir = '/home/smooge'
+##
+## These are global variables for hte GEOIP and need to be changed per site. 
+GEO_DB_COUNTRY_LOC = basedir + '/GeoIP2/GeoIP2-Country.mmdb'
+GEO_DB_DOMAIN_LOC  = basedir + '/GeoIP2/GeoIP2-Domain.mmdb'
+GEO_DB_ISP_LOC     = basedir + '/GeoIP2/GeoIP2-ISP.mmdb'
+
+##
+## Our one mirror.
 MIRROR_LIST="""
 http://www.smoogespace.com/downloads/census/
 """
 
+##
+## The logfile 
 LOGFILE='/home/smooge/census-server/GBCC_server.log'
