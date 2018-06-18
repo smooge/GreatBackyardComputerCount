7b0f3ff99acf4e486618852b4c8b5f64a9097480
Stephen Smoogen <smooge@gmail.com>
2018-06-18 19:46:57 +0000

    still doing catchup from destroyed repo

---
+++ census-server/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='census-server',
+      version='0.1',
+      description='Simplified census server for yum based systems',
+      url='http://github.com/smooge/GreatBackyardComputerCount/census-server',
+      author='Stephen Smoogen',
+      author_email='smooge@smoogespace.com',
+      license='GPL v2',
+      packages=['census-server'],
+      zip_safe=False)
