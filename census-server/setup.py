d1ebf17edeac659a8d293538a41cfa49c7afa902
Stephen Smoogen <smooge@gmail.com>
2018-06-18 20:00:09 +0000

    initial commit for the service

--- census-server/setup.py
+++ census-server/setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 setup(name='census-server',
       version='0.1',
       description='Simplified census server for yum based systems',
-      url='http://github.com/smooge/GreatBackyardComputerCount/census-server',
+      url='http://github.com/smooge/GreatBackyardComputerCount/',
       author='Stephen Smoogen',
       author_email='smooge@smoogespace.com',
+      maintainer='Stephen Smoogen',
+      maintainer_email='smooge@smoogespace.com',
       license='GPL v2',
       packages=['census-server'],
       zip_safe=False)
